da0044338f0e9eac9bc93e1b9775c55e7b0232eb
bagool185 <ioan.adrian98@gmail.com>
2017-07-20 11:14:08 +0300

    Update CRPG.cpp

--- CRPG.cpp
+++ CRPG.cpp
@@ -12,18 +12,15 @@
 
 using namespace std;
 
-int main(void)
-{
+int main(void) {
 #pragma region Instructions
-
-	/* Some sort of a user manual with weird combinations of colors*/
-
+	/* Some sort of user manual with weird combinations of colors*/
 	HANDLE hColor = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	SetConsoleTextAttribute(hColor, 193);
 
 	cout << "Instructions:\n\n";
-	cout << "\tYou'll find yourself in the role of a " << (char)PLAYER << " and will need to reach the ! .\n";
+	cout << "\tYou'll find yourself in the role of a " << PLAYER << " and will need to reach the ! .\n";
 	cout << "\tThe final score depends on your accuracy and speed.\n\n";
 
 	SetConsoleTextAttribute(hColor, 232);
@@ -36,24 +33,23 @@ int main(void)
 
 	cout << "Legend:\n\n";
 	cout << "\t~~~ = water (If you happen to touch it, you'll drown) \n\t";
-	cout << (char)BRIDGE << (char)BRIDGE  << (char)BRIDGE << " = bridge\n\t";
-	cout << (char)GRASS  << (char)GRASS   << (char)GRASS  << " = grass\n\n\t";
-	cout << (char)FENCE  << (char)FENCE   << (char)FENCE  << " = fence\n\n\t";
-	cout << (char)MOBS   << " = mobs (You can pass through them, but you'll lose 20 health)\n\n\n";
+	cout << BRIDGE << BRIDGE  << BRIDGE << " = bridge\n\t";
+	cout << GRASS  << GRASS   << GRASS  << " = grass\n\n\t";
+	cout << FENCE  << FENCE   << FENCE  << " = fence\n\n\t";
+	cout << MOBS   << " = mobs (You can pass through them, but you'll lose 20 health)\n\n\n";
 
 	SetConsoleTextAttribute(hColor, 185);
 	cout << "Press any key to begin. Have fun!";
 
-	system("pause > nul");
-
+	PAUSE
 	/* Instructions endregion*/
 #pragma endregion
 
 #pragma region Game Setup	
 
-	_initMap();
-	_init();
-	_runGame();
+	initMap();
+	init();
+	runGame();
 
 	return 0;
 	/* Game Setup endregion */
@@ -62,19 +58,17 @@ int main(void)
 
 #pragma region Functions
 
-inline void	    _gotoxy(short x, short y)		// sets the cursor to the coordinates x, y	
-{
-	COORD p = { x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
-}
-
-inline bool     _exists(const string& name) {
+inline bool exists(const string& name) {
 	struct stat buffer;
 	return (stat(name.c_str(), &buffer) == 0);
 }
 
-inline void		_repositionConsole()			// repositions the console depending on desktop's size
-{
+inline void	gotoxy(short x, short y) {	// sets the cursor to the coordinates x, y	
+	COORD p = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
+}
+
+inline void	repositionConsole() {			// repositions the console depending on desktop's size
 	HWND    consoleWindow = GetConsoleWindow();
 
 	int		horizontalRes;
@@ -92,8 +86,7 @@ inline void		_repositionConsole()			// repositions the console depending on desk
 	SetWindowPos(consoleWindow, 0, horizontalRes, verticalRes, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 }
 
-inline void		_resizeConsole()			// resizes the console
-{
+inline void	resizeConsole()	{	// resizes the console
 	SMALL_RECT	r;
 	COORD	    c;
 	HANDLE		hConOut = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -110,8 +103,7 @@ inline void		_resizeConsole()			// resizes the console
 	SetConsoleScreenBufferSize(hConOut, c);
 }
 
-inline void     _mobCollision()		// Mob Collision handler
-{
+inline void mobCollision() {	// Mob Collision handler
 	health -= 20;
 
 	gameSpeed += 34.33;
@@ -119,16 +111,14 @@ inline void     _mobCollision()		// Mob Collision handler
 	BEEP_ENEMY
 }
 
-inline bool		_enemyNearby()		// checks whether there is any enemy nearby
-{
+inline bool enemyNearby() {		// checks whether there is any enemy nearby
 	return (Map[pY + 1][pX] == MOBS ||
 		Map[pY][pX + 1] == MOBS ||
 		Map[pY - 1][pX] == MOBS ||
 		Map[pY][pX - 1] == MOBS);
 }
 
-inline void		_die(char how)
-{
+inline void	die(char how) {
 	CLEAR_SCREEN
 
 	isRunning = false;
@@ -137,8 +127,7 @@ inline void		_die(char how)
 
 	cout << "Sorry, you've ";
 
-	switch (how)
-	{
+	switch (how) {
 	case 'w':
 		cout << "drowned!\n";
 		break;
@@ -157,25 +146,22 @@ inline void		_die(char how)
 	cin.ignore();
 }
 
-inline void		_hideCaret()
-{
+inline void	hideCaret() {
 	// prevents the Caret from blinking so it won't distors the image
 	CONSOLE_CURSOR_INFO cursor = { 1, FALSE };
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
 }
 
-inline void		_init()			// initializes the game
-{
+inline void	init() {			// initializes the game
 	SetConsoleTitle(_T("CRPG powered by AIB Inc.™"));
-	_repositionConsole();
-	_resizeConsole();
-	_hideCaret();
+	repositionConsole();
+	resizeConsole();
+	hideCaret();
 
 	system("COLOR 1F");	 // sets the background green and the foreground white	
 }
 
-inline void		_win()
-{
+inline void	win() {
 	CLEAR_SCREEN
 
 	isRunning = false;		// stops the execution of the loop
@@ -210,41 +196,41 @@ inline void		_win()
 	scores.close();
 }
 
-void	_buildMap()
-{
+void buildMap() {
 	string	line;
 
-	if (exists("firstLevel.map"))
-	{
+	if (exists("firstLevel.map")) {
 		ifstream mapReader("firstLevel.map");
 
-		while (getline(mapReader, line))		// read the map, line by line
-		{
+		while (getline(mapReader, line)) {		// read the map, line by line
 			tempMap.push_back(line);
+			HEIGHT++;
+			WIDTH = line.size();
+		}
+
+		isBridge = new bool*[HEIGHT];
+
+		for (short i = 0; i < HEIGHT; i++) {
+			isBridge[i] = new bool[WIDTH];
 		}
 
 		mapReader.close();
 	}
-	else
-	{
+	else {
 		CLEAR_SCREEN
 		cout << "The map file is not in the same directory with the .exe. Please provide it in order to play";
-		system("pause > nul");
+		PAUSE
 	}	
 }
 
-void	_initMap()
-{
-	_buildMap();
+void initMap() {
+	buildMap();
 
 	Map = tempMap;
 
-	for (short h = 0; h < HEIGHT; h++)		// replaces every single character from the extended ASCII table
-	{
-		for (short w = 0; w < WIDTH; w++)
-		{
-			switch (MapHW)
-			{
+	for (short h = 0; h < HEIGHT; h++) {	// replaces every single character from the extended ASCII table
+		for (short w = 0; w < WIDTH; w++) {
+			switch (MapHW) {
 			case '#': MapHW = WALL;   break;
 			case '@': MapHW = PLAYER; break;
 			case 'f': MapHW = FENCE;  break;
@@ -262,8 +248,7 @@ void	_initMap()
 	}
 }
 
-void	_runGame()
-{
+void runGame() {
 	system("COLOR 1F");
 
 	start = clock();
@@ -280,298 +265,264 @@ void	_runGame()
 
 	CLEAR_SCREEN
 
-		while (isRunning)
-		{
+	while (isRunning) {
 
 #pragma region Main Output
 
-			/* Overwrites the map, over and over */
-			for (short h = 0; h < HEIGHT; h++)
-			{
-				/*	sets the cursor to the beginning of every line so it would overwrite */
-				_gotoxy(0, h);
-				cout << "\t" << Map[h] << "\n" << flush;
-			}
+		/* Overwrites the map, over and over */
+		for (short h = 0; h < HEIGHT; h++) {
+			/*	sets the cursor to the beginning of every line so it would overwrite */
+			gotoxy(0, h);
+			cout << "\t" << Map[h] << "\n" << flush;
+		}
 
-			cout << "\n \n \n";
+		cout << "\n \n \n";
 
-			cout << "\tHEALTH: " << health << "/100 ";
+		cout << "\tHEALTH: " << health << "/100 ";
 
-			seconds = (clock() - start) / 1000;		// timer
-			cout << "\n\tTIME: " << seconds << " seconds";
+		seconds = (clock() - start) / 1000;		// timer
+		cout << "\n\tTIME: " << seconds << " seconds";
 
 #pragma endregion 
 
 #pragma region Controls
 
 #pragma region Space
+		// if SPACE is pressed
+		if SPACE {
+			// checks whether there is any enemy nearby
+			if (enemyNearby()) {	
+				if (Map[pY + 1][pX] == MOBS) {
+					Map[pY + 1][pX] = WHITESPACE;
+					threatsEliminated++;
 
-			if SPACE	// if SPACE is pressed
-			{
-				if (_enemyNearby())		// checks whether there is any enemy nearby
-				{
-					if (Map[pY + 1][pX] == MOBS)
-					{
-						Map[pY + 1][pX] = WHITESPACE;
-						threatsEliminated++;
+					BEEP_POINT
+				}
+				if (Map[pY - 1][pX] == MOBS) {
+					Map[pY - 1][pX] = WHITESPACE;
+					threatsEliminated++;
 
-						BEEP_POINT
-					}
+					BEEP_POINT
+				}
+				if (Map[pY][pX + 1] == MOBS) {
+					Map[pY][pX + 1] = WHITESPACE;
+					threatsEliminated++;
 
-					if (Map[pY - 1][pX] == MOBS)
-					{
-						Map[pY - 1][pX] = WHITESPACE;
-						threatsEliminated++;
+					BEEP_POINT
+				}
+				if (Map[pY][pX - 1] == MOBS) {
+					Map[pY][pX - 1] = WHITESPACE;
+					threatsEliminated++;
 
-						BEEP_POINT
-					}
-
-					if (Map[pY][pX + 1] == MOBS)
-					{
-						Map[pY][pX + 1] = WHITESPACE;
-						threatsEliminated++;
-
-						BEEP_POINT
-					}
-
-					if (Map[pY][pX - 1] == MOBS)
-					{
-						Map[pY][pX - 1] = WHITESPACE;
-						threatsEliminated++;
-
-						BEEP_POINT
-					}
+					BEEP_POINT
 				}
 			}
+		}
 
 #pragma endregion
 
 #pragma region UP
+		// if up arrow key is pressed
+		if UP {
+			switch (Map[pY - 1][pX]) {
+			// if the character above the player is whitespace, then it goes there
+			case WHITESPACE:							
+				Map[pY - 1][pX] = PLAYER;
+				Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
 
-				if UP		// if up arrow key is pressed
-				{
-					switch (Map[pY - 1][pX])
-					{
-					case WHITESPACE:							// if the character above the player is whitespace, then it goes there
-						Map[pY - 1][pX] = PLAYER;
-						Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				pY--;
+				break;
+			// if the character above the player is !, then he finishes the game
+			case GOAL:									
+				win();
+				break;
+			// if the player falls into water, he dies
+			case WATER:									
+				die('w');
+				break;
+			case BRIDGE:
+				Map[pY - 1][pX] = PLAYER;
+				Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
 
-						pY--;
-						break;
+				pY--;
+				break;
+			// if the player bumps into a mob, then the mob will disappear, but at the cost of losing 20 health
+			case MOBS:									
+				mobCollision();
 
-					case GOAL:									// if the character above the player is !, then he finishes the game
-						_win();
-						break;
-
-					case WATER:									// if the player falls into water, he dies
-						_die('w');
-						break;
-
-					case BRIDGE:
-						Map[pY - 1][pX] = PLAYER;
-						Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
-
-						pY--;
-						break;
-
-					case MOBS:									// if the player bumps into a mob, then the mob will disappear, but at the cost of losing 20 health
-
-						_mobCollision();
-
-						if (health == 0)
-						{
-							_die('k');
-						}
-						else
-						{
-							Map[pY - 1][pX] = PLAYER;
-							Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
-						}
-
-						pY--;
-						break;
-
-
-					default:
-						break;
-					}
+				if (health == 0) {
+					die('k');
 				}
+				else {
+					Map[pY - 1][pX] = PLAYER;
+					Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				}
+
+				pY--;
+				break;
+			default: break;
+			}
+		}
 
 #pragma endregion		
 
 #pragma region RIGHT
 
-					/* The rest are the same */
+		/* The rest are the same */
+		// if RIGHT arrow key is pressed
+		if RIGHT {
+			switch (Map[pY][pX + 1]) {
+			case WHITESPACE:
+				Map[pY][pX + 1] = PLAYER;
 
-					if RIGHT 	// if RIGHT arrow key is pressed
-					{
-						switch (Map[pY][pX + 1])
-						{
-						case WHITESPACE:
-							Map[pY][pX + 1] = PLAYER;
+				Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
 
-							Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				pX++;
+				break;
 
-							pX++;
-							break;
+			case GOAL:
+				win();
+				break;
 
-						case GOAL:
-							_win();
-							break;
+			case WATER:
+				die('w');
+				break;
 
-						case WATER:
-							_die('w');
-							break;
+			case BRIDGE:
+				Map[pY][pX + 1] = PLAYER;
 
-						case BRIDGE:
-							Map[pY][pX + 1] = PLAYER;
+				Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
 
-							Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				pX++;
 
-							pX++;
+				break;
 
-							break;
+			case MOBS:
 
-						case MOBS:
+				mobCollision();
 
-							_mobCollision();
+				if (health == 0) {
+					die('k');
+				}
+				else {
+					Map[pY][pX + 1] = PLAYER;
 
-							if (health == 0)
-							{
-								_die('k');
-							}
-							else
-							{
-								Map[pY][pX + 1] = PLAYER;
+					Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				}
 
-								Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
-							}
+				pX++;
+				break;
 
-							pX++;
-							break;
-
-						default:
-							break;
-						}
-					}
+			default: break;
+			}
+		}
 
 #pragma endregion
 
 #pragma region LEFT
 
-						if LEFT
-						{
-							switch (Map[pY][pX - 1])
-							{
-							case WHITESPACE:
-								Map[pY][pX - 1] = PLAYER;
+		if LEFT	{
+			switch (Map[pY][pX - 1]) {
+			case WHITESPACE:
+				Map[pY][pX - 1] = PLAYER;
 
-								Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
 
-								pX--;
-								break;
+				pX--;
+				break;
 
-							case GOAL:
-								_win();
-								break;
+			case GOAL:
+				win();
+				break;
 
-							case WATER:
-								_die('w');
-								break;
+			case WATER:
+				die('w');
+				break;
 
-							case BRIDGE:
-								Map[pY][pX - 1] = PLAYER;
+			case BRIDGE:
+				Map[pY][pX - 1] = PLAYER;
 
-								Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
 
-								pX--;
-								break;
+				pX--;
+				break;
 
-							case MOBS:
+			case MOBS:
+				mobCollision();
 
-								_mobCollision();
+				if (health == 0) {
+					die('k');
+				}
+				else {
+					Map[pY][pX - 1] = PLAYER;
+					Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				}
 
-								if (health == 0)
-								{
-									_die('k');
-								}
-								else
-								{
-									Map[pY][pX - 1] = PLAYER;
-									Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
-								}
+				pX--;
+				break;
 
-								pX--;
-								break;
-
-							default:
-								break;
-							}
-						}
+			default:
+				break;
+			}
+		}
 
 #pragma endregion
 
 #pragma region DOWN
 
-							if DOWN
-							{
-								switch (Map[pY + 1][pX])
-								{
-								case WHITESPACE:
-									Map[pY + 1][pX] = PLAYER;
-									Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+		if DOWN	{
+			switch (Map[pY + 1][pX]) {
+			case WHITESPACE:
+				Map[pY + 1][pX] = PLAYER;
+				Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
 
-									pY++;
-									break;
+				pY++;
+				break;
 
-								case GOAL:
-									_win();
-									break;
+			case GOAL:
+				win();
+				break;
 
-								case WATER:
-									_die('w');
-									break;
+			case WATER:
+				die('w');
+				break;
 
-								case BRIDGE:
-									Map[pY + 1][pX] = PLAYER;
-									Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+			case BRIDGE:
+				Map[pY + 1][pX] = PLAYER;
+				Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
 
-									pY++;
-									break;
+				pY++;
+				break;
 
-								case MOBS:
+			case MOBS:
+				mobCollision();
 
-									_mobCollision();
+				if (health == 0) {
+					die('k');
+				}
+				else {
+					Map[pY + 1][pX] = PLAYER;
+					Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
+				}
 
-									if (health == 0)
-									{
-										_die('k');
-									}
-									else
-									{
-										Map[pY + 1][pX] = PLAYER;
-										Map[pY][pX] = (isBridge[pY][pX] == true) ? BRIDGE : WHITESPACE;
-									}
+				pY++;
+				break;
 
-									pY++;
-									break;
-
-								default:
-									break;
-								}
-							}
-
-#pragma endregion
-
-#pragma endregion
-
-						Sleep(gameSpeed);
+			default:
+				break;
+			}
 		}
 
-	if (res == 'r')
-	{
+#pragma endregion
+
+#pragma endregion
+
+		Sleep(gameSpeed);
+	}
+
+	if (res == 'r')	{
 		res = '\0';
-		_runGame();
+		runGame();
 	}
 
 	return;
